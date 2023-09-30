4b0f95448de9af6bb0a977e6e456ebf23e9218a2
trannhatcoder <143916621+trannhatcoder@users.noreply.github.com>
2023-09-30 16:49:49 +0700

    chapter7-8-1

---
+++ HocPHP/ch07_ex1/display_results.php
@@ -0,0 +1,70 @@
+<?php
+    // get the data from the form
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $password = filter_input(INPUT_POST, 'password');
+    $phone = filter_input(INPUT_POST, 'phone');
+
+    if(!isset($email)==false){
+        $email = 'Email did not correct or not valid';
+    }
+
+    // get the rest of the data for the form
+
+    // for the heard_from radio buttons,
+    // display a value of 'Unknown' if the user doesn't select a radio button
+    $heard_from = filter_input(INPUT_POST, 'heard_from');
+    if($heard_from === NULL){
+        $heard_from = 'Unknow';
+    }
+
+    $wants_updates = filter_input(INPUT_POST, 'wants_updates');
+    // $wants_updates = $_POST['wants_updates'];
+    if(isset($wants_updates)){
+        $wants_updates = 'YES';
+    }
+    else{
+        $wants_updates = 'NO';
+    }
+
+    $contact_via = filter_input(INPUT_POST, 'contact_via');
+
+    $comments = filter_input(INPUT_POST, 'comments');
+    $comments = htmlspecialchars($comments);
+    $comments = nl2br($comments, true);
+
+    // for the wants_updates check box,
+    // display a value of 'Yes' or 'No'
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Account Information</title>
+    <link rel="stylesheet" type="text/css" href="main.css"/>
+</head>
+<body>
+    <main>
+        <h1>Account Information</h1>
+
+        <label>Email Address:</label>
+        <span><?php echo htmlspecialchars($email); ?></span><br>
+
+        <label>Password:</label>
+        <span><?php echo $password;?></span><br>
+
+        <label>Phone Number:</label>
+        <span><?php echo $phone;?></span><br>
+
+        <label>Heard From:</label>
+        <span><?php echo $heard_from; ?></span><br>
+
+        <label>Send Updates:</label>
+        <span><?php echo $wants_updates; ?></span><br>
+
+        <label>Contact Via:</label>
+        <span><?php echo $contact_via; ?></span><br><br>
+
+        <span>Comments:</span><br>
+        <span><?php echo $comments;?></span><br>        
+    </main>
+</body>
+</html>
